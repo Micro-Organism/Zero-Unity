e71bf0629d5de73cbdd1b7743e401bf2a3888e96
Micro-Organism <coolrabbit1993@gmail.com>
2024-12-09 19:19:20 +0800

    feat: springboot 集成 unity

---
+++ zero-unity-boot/src/test/java/com/zero/unity/ZeroUnityBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.unity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroUnityBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
